a7393cc1c1a10eb707d1b5344a0ca5bd5d97f653
kuldeep-IT <rathod99244@gmail.com>
2024-03-16 18:43:39 +0530

    home page and route setup

--- src/App.js
+++ src/App.js
@@ -5,17 +5,38 @@ import Footer from "./layout/Footer";
 import AuthForm from "./pages/AuthForm";
 // import CourseUpload from "./components/CourseUpload";
 
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Homepage from "./pages/Homepage";
+
 function App() {
   return (
-    <div className="App">
-      {/* <h1>Hello World</h1> */}
-      {/* <CourseUpload /> */}
-      {/* <CourseCard />
-      <ConnectButton />
-      <Footer /> */}
+    // <div className="App">
+    //   {/* <h1>Hello World</h1> */}
+    //   {/* <CourseUpload /> */}
+    //   {/* <CourseCard />
+    //   <ConnectButton />
+    //   <Footer /> */}
 
-      <AuthForm />
-    </div>
+    //   <AuthForm />
+    // </div>
+    <BrowserRouter>
+      <div>
+        {/* Navigation Links */}
+        {/* <nav>
+          <Link to="/">Home</Link> |
+          <Link to="/about">About</Link> |
+          <Link to="/contact">Contact</Link>
+        </nav> */}
+
+        {/* Route Configuration */}
+        <Routes>
+          <Route path="/" element={<AuthForm />} />
+          <Route path="/home" element={<Homepage />} />
+          {/* <Route path="/about" element={<AboutScreen />} />
+          <Route path="/contact" element={<ContactScreen />} /> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Homepage.js
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material"
+
+
+const Homepage = () => {
+    return (
+        <Box>Hiiii</Box>
+    )
+}
+
+export default Homepage
